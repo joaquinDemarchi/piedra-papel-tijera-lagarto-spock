13fbdca216d6a5401614a1274d698557cd512d76
Joaquin Casa Ubuntu <joa.demarchi@gmail.com>
2024-02-08 21:09:21 -0300

    agregando detalle a victorias

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ document.getElementById('gameForm').addEventListener('submit', function(event) {
   const usuario = document.getElementById('usuario').value;
   const maquina  = ['piedra', 'papel', 'tijera', 'spock', 'lagarto'];
   const eleccionMaquina = getRandomInt(maquina);
+  let detalle = '';
 
   let resultadoTexto = '';
 
-  const mensajePerdedor = 'Perdiste¡ Maldito Sheldon.'
+  const mensajePerdedor = 'Perdiste... Maldito Sheldon.'
   const mensajeGanador = 'Ganaste¡ Bien hecho.'
   
   if (maquina[eleccionMaquina] == usuario){
       resultadoTexto = 'Empataron!';
+      detalle = 'Tu y Sheldon estan conectados...'
   } else if (usuario == 'tijera' ){
       if (maquina[eleccionMaquina] != 'piedra' && maquina[eleccionMaquina] != 'spock') {
           resultadoTexto = mensajeGanador;
@@ -48,7 +50,72 @@ document.getElementById('gameForm').addEventListener('submit', function(event) {
       }
   }
 
-  document.getElementById('resultado').innerText = 'Sheldon ha elegido \n '+ maquina[eleccionMaquina] +' \n'+ resultadoTexto;
+  
+  if (
+    //piedra-papel
+    (usuario == 'papel' && maquina[eleccionMaquina] == 'tijera' )||
+    (usuario == 'tijera' && maquina[eleccionMaquina] == 'papel')
+  ){
+    detalle = '"Papel envuelve piedra"'
+  } else  if (
+    //piedra-tijera
+    (usuario == 'piedra' && maquina[eleccionMaquina] == 'tijera' )||
+    (usuario == 'tijera' && maquina[eleccionMaquina] == 'piedra')
+  ){
+    detalle = '"Pidra destruye tijera"'
+  } else  if (
+    //papel-tijera
+    (usuario == 'papel' && maquina[eleccionMaquina] == 'tijera' )||
+    (usuario == 'tijera' && maquina[eleccionMaquina] == 'papel')
+  ){
+    detalle = '"Tijera corta papel"'
+  } else  if (
+    //spock-tijera
+    (usuario == 'spock' && maquina[eleccionMaquina] == 'tijera' )||
+    (usuario == 'tijera' && maquina[eleccionMaquina] == 'spock')
+  ){
+    detalle = '"Spock rompe tijeras"'
+  } else  if (
+    //spock-papel
+    (usuario == 'spock' && maquina[eleccionMaquina] == 'papel' )||
+    (usuario == 'papel' && maquina[eleccionMaquina] == 'spock')
+  ){
+    detalle = '"Papel desautoriza a Spock"'
+  } else  if (
+    //spock-piedra
+    (usuario == 'spock' && maquina[eleccionMaquina] == 'piedra' )||
+    (usuario == 'piedra' && maquina[eleccionMaquina] == 'spock')
+  ){
+    detalle = '"Spock vaporiza piedra."'
+  } else  if (
+    //spock-lagarto
+    (usuario == 'spock' && maquina[eleccionMaquina] == 'lagarto' )||
+    (usuario == 'lagarto' && maquina[eleccionMaquina] == 'spock')
+  ){
+    detalle = '"Lagarto envenena a Spock"'
+  } else  if (
+    //tijera-lagarto
+    (usuario == 'tijera' && maquina[eleccionMaquina] == 'lagarto' )||
+    (usuario == 'lagarto' && maquina[eleccionMaquina] == 'tijera')
+  ){
+    detalle = '"Tijeras decapitan a Lagarto"'
+  } else  if (
+    //papel-lagarto
+    (usuario == 'papel' && maquina[eleccionMaquina] == 'lagarto' )||
+    (usuario == 'lagarto' && maquina[eleccionMaquina] == 'papel')
+  ){
+    detalle = '"Lagarto devora papel"'
+  } else  if (
+    //piedra-lagarto
+    (usuario == 'piedra' && maquina[eleccionMaquina] == 'lagarto' )||
+    (usuario == 'lagarto' && maquina[eleccionMaquina] == 'piedra')
+  ){
+    detalle = '"Piedra aplasta a lagarto"'
+  }
+  
+
+
+  document.getElementById('resultado').innerText = 'Sheldon ha elegido \n '+ maquina[eleccionMaquina]  +' \n'+ detalle +' \n'+ resultadoTexto;
 });
 
 console.log('jdks');
